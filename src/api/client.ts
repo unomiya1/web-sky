d6941bd482b4d4c7c197512d1bc239c73a5648e4
Sky Pictures 2 <skypictures@192.168.1.29>
2026-07-14 14:48:46 +0700

    ubah foto about us

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import type { HomepageData } from '../types/homepage'
-import aboutUsImage from '../assets/about us/about us.jpg'
+import aboutUsImage from '../assets/about us/about us1.png'
 
 const BASE_PATH = import.meta.env.BASE_URL
 const api = axios.create({
